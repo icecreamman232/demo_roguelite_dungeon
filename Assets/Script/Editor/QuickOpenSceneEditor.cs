b8cf4159196b132296b81dce430582ba42766a9d
Stephen Ho <stephen.ho.232@gmail.com>
2025-07-03 01:48:42 +0700

    Adding quick scene selection menu

---
+++ Assets/Script/Editor/QuickOpenSceneEditor.cs
@@ -0,0 +1,32 @@
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+namespace SGGames.Script.EditorExtensions
+{
+    public class QuickOpenSceneEditor
+    {
+        [MenuItem("SGGames/Scene/Bootstrap")]
+        public static void OpenBootstrapScene()
+        {
+            OpenScene("Assets/Scenes/BootstrapScene.unity");
+        }
+        
+        [MenuItem("SGGames/Scene/MainMenu")]
+        public static void OpenMainMenuScene()
+        {
+            OpenScene("Assets/Scenes/MainMenuScene.unity");
+        }
+        
+        [MenuItem("SGGames/Scene/Gameplay")]
+        public static void OpenGameplayScene()
+        {
+            OpenScene("Assets/Scenes/GameplayScene.unity");
+        }
+        
+        private static void OpenScene(string address)
+        {
+            EditorSceneManager.OpenScene(address, OpenSceneMode.Single);
+        }
+    }
+}
+
