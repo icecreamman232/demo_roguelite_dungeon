b93e2a947c3b658c0669b0a1c4e9cbdfd1bce4df
Stephen Ho <stephen.ho.232@gmail.com>
2025-07-07 01:03:29 +0700

    Adding wizard to create enemy brain in hierachy window

--- Assets/Script/Editor/QuickMenuEditor.cs
+++ Assets/Script/Editor/QuickMenuEditor.cs
@@ -1,102 +1,28 @@
-using System.Reflection;
-using SGGames.Script.Core;
-using SGGames.Script.Entity;
-using SGGames.Script.Events;
-using SGGames.Script.HealthSystem;
 using SGGames.Scripts.Entity;
 using UnityEditor;
 using UnityEngine;
 
-
 namespace SGGames.Script.EditorExtensions
 {
-    public class QuickMenuEditor 
+    public partial class QuickMenuEditor
     {
-        [MenuItem("SGGames/Create Enemy Wizard")]
-        public static void CreateEnemyPlaceHolderInScene()
+        [MenuItem("GameObject/Create Enemy Brain",priority = 0)]
+        public static void AddingEmptyEnemyBrain()
         {
-            GameObject newEnemyGO = new GameObject();
-            newEnemyGO.name = "New Enemy";
-            newEnemyGO.layer = LayerManager.EnemyLayer;
-
-            AddMainCollisionComponents(newEnemyGO);
-            AddEnemyController(newEnemyGO);
-            AddHealthComponent(newEnemyGO);
-            AddMovementComponent(newEnemyGO);
-
-            AddModel(newEnemyGO);
-            AddBodyDamageGameObject(newEnemyGO);
-
-
-            AddSharedPrefab(newEnemyGO, "Assets/Prefab/UI/EnemyHealthBar.prefab");
-            AddSharedPrefab(newEnemyGO, "Assets/Prefab/Module/FillOverlayColorOnSprite.prefab");
-        }
-
-        private static void AddMainCollisionComponents(GameObject main)
-        {
-            //Collision components
-            main.AddComponent<BoxCollider2D>();
-            var rigidBody2D = main.AddComponent<Rigidbody2D>();
-            rigidBody2D.bodyType = RigidbodyType2D.Kinematic;
-            rigidBody2D.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
-            rigidBody2D.sleepMode = RigidbodySleepMode2D.NeverSleep;
-        }
-
-        private static void AddEnemyController(GameObject main)
-        {
-            var controller = main.AddComponent<EnemyController>();
-            var gameEventAsset = AssetDatabase.LoadAssetAtPath("Assets/Data/Events/Game Event.asset", typeof(GameEvent));
-            var gameEventField = typeof(EnemyController).GetField("m_gameEvent", BindingFlags.NonPublic | BindingFlags.Instance);
-            gameEventField.SetValue(controller, gameEventAsset);
+            GameObject brain = new GameObject();
+            brain.name = "New Empty Brain";
+            brain.AddComponent<EnemyBrain>();
             
-            var permissionField = typeof(EnemyController).GetField("m_isPermit", BindingFlags.NonPublic | BindingFlags.Instance);
-            permissionField.SetValue(controller,true);
-        }
-
-        private static void AddHealthComponent(GameObject main)
-        {
-            var health = main.AddComponent<EnemyHealth>();
-            var healthPermissionField = typeof(EnemyHealth).GetField("m_isPermit", BindingFlags.NonPublic | BindingFlags.Instance);
-            healthPermissionField.SetValue(health,true);
-        }
-
-        private static void AddMovementComponent(GameObject main)
-        {
-            var movement = main.AddComponent<EnemyMovement>();
-            var movementPermissionField = typeof(EnemyMovement).GetField("m_isPermit", BindingFlags.NonPublic | BindingFlags.Instance);
-            movementPermissionField.SetValue(movement,true);
-        }
-
-        private static void AddModel(GameObject main)
-        {
-            GameObject enemyModelGO = new GameObject();
-            enemyModelGO.name = "Model";
-            enemyModelGO.transform.parent = main.transform;
-            var spriteRenderer = enemyModelGO.AddComponent<SpriteRenderer>();
-            spriteRenderer.sortingLayerName = "Enemy";
-        }
-
-        private static void AddBodyDamageGameObject(GameObject main)
-        {
-            GameObject bodyDamageGO = new GameObject();
-            bodyDamageGO.name = "BodyDamage";
-            bodyDamageGO.transform.parent = main.transform;
-            bodyDamageGO.layer = LayerManager.EnemyLayer;
+            GameObject selection = Selection.activeGameObject;
+            brain.transform.SetParent(selection.transform);
             
-            var dmgHandler = bodyDamageGO.AddComponent<DamageHandler>();
-            var targetMaskField = typeof(DamageHandler).GetField("m_targetMask", BindingFlags.NonPublic | BindingFlags.Instance);
-            targetMaskField.SetValue(dmgHandler,(LayerMask)LayerManager.PlayerMask);
-
-            var bodyCollider = bodyDamageGO.AddComponent<BoxCollider2D>();
-            bodyCollider.isTrigger = true;
-        }
-
-        private static void AddSharedPrefab(GameObject main, string path)
-        {
-            var enemyHPBarPrefab = AssetDatabase.LoadAssetAtPath(path,typeof(GameObject)) as GameObject;
-            var enemyHPBar = PrefabUtility.InstantiatePrefab(enemyHPBarPrefab);
-            ((GameObject)enemyHPBar).transform.parent = main.transform;
+            GameObject action = new GameObject();
+            action.name = "Actions";
+            action.transform.SetParent(brain.transform);
+            
+            GameObject decision = new GameObject();
+            decision.name = "Decisions";
+            decision.transform.SetParent(brain.transform);
         }
     }
 }
-
