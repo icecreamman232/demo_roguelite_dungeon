d40748db7a0e9c470030fbec665109c00246f2e4
Stephen Ho <stephen.ho.232@gmail.com>
2025-07-22 00:55:31 +0700

    Adding weight value

---
+++ Assets/Script/Data/WeightSheet.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+namespace SGGames.Script.Data
+{
+    [Serializable]
+    public class WeightSheet
+    {
+        [SerializeField] private string m_id;
+        [SerializeField] private WeightValue[] m_weightSheet;
+        
+        public string Id => m_id;
+        public WeightValue GetWeightValue(int index) => m_weightSheet[index];
+        
+        public void CalculatePercentage()
+        {
+            var totalWeight = 0f;
+            foreach (var weight in m_weightSheet)
+            {
+                totalWeight += weight.Weight;
+            }
+            
+            var currentPercentage = 0f;
+            
+            foreach (var weight in m_weightSheet)
+            {
+                weight.LowerPercentage = currentPercentage;
+                weight.UpperPercentage = currentPercentage + weight.Weight / totalWeight * 100f;
+                currentPercentage += weight.Weight / totalWeight * 100f;
+            }
+        }
+    }
+}
---
+++ Assets/Script/Data/WeightValue.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using SGGames.Scripts.EditorExtensions;
+using UnityEngine;
+
+namespace SGGames.Script.Data
+{
+    [Serializable]
+    public class WeightValue
+    {
+        [SerializeField] private string m_id;
+        [SerializeField] private float m_weight;
+        [SerializeField][ReadOnly] private float m_lowerPercentage;
+        [SerializeField][ReadOnly] private float m_upperPercentage;
+        
+        public string Id => m_id;
+        public float Weight => m_weight;
+        public float LowerPercentage
+        {
+            get => m_lowerPercentage;
+            set => m_lowerPercentage = value;
+        }
+        public float UpperPercentage
+        {
+            get => m_upperPercentage;
+            set => m_upperPercentage = value;
+        }
+    }
+}
