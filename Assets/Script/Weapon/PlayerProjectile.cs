2a7be10b6723867e1f15b2a74a33551dd4c8153e
Stephen Ho <stephen.ho.232@gmail.com>
2025-08-06 22:28:37 +0700

    Adding special hit vfx

--- Assets/Script/Weapon/PlayerProjectile.cs
+++ Assets/Script/Weapon/PlayerProjectile.cs
@@ -1,7 +1,36 @@
+using System.Collections;
+using UnityEngine;
+
 namespace SGGames.Script.Weapons
 {
     public class PlayerProjectile : Projectile
     {
+        [SerializeField] private Animator m_animator;
         
+        private readonly float m_impactAnimDuration = 0.5f;
+        private readonly int k_ANIM_IMPACT_PARAM = Animator.StringToHash("Trigger_Impact");
+
+        private void OnEnable()
+        {
+            m_projectileCollider.enabled = true;
+        }
+        
+        protected override void DestroyProjectile()
+        {
+            StartCoroutine(DestroyProjectileCoroutine());
+        }
+
+        private IEnumerator DestroyProjectileCoroutine()
+        {
+            m_projectileCollider.enabled = false;
+            m_isAlive = false;
+            transform.rotation = Quaternion.identity;
+            if (m_animator != null)
+            {
+                m_animator.SetTrigger(k_ANIM_IMPACT_PARAM);
+                yield return new WaitForSeconds(m_impactAnimDuration);
+            }
+            this.gameObject.SetActive(false);
+        }
     }
 }
